400343deda05fbce3f1c5050f66bf1916e4fda89
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-30 16:21:07 -0300

    Added ids to all folders

--- src/models/folder.model.ts
+++ src/models/folder.model.ts
@@ -1,17 +1,20 @@
 import { IFilter } from "./filter.model";
 
 export interface IRecentlyMovedFolder {
+  id: string;
   name: string;
   origin: string;
   destination: string;
   time: Date;
 }
 export interface IOriginFolder {
+  id: string;
   name: string;
   path: string;
   date: Date;
 }
 export interface IDestinationFolder {
+  id: string;
   folder: string;
   path: string;
   date: Date | string;
