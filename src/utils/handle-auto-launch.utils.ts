4f2ae069d2c019cc1eca77efb44cea29feadc995
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-01-05 21:39:15 -0300

    Refactor from const to named functions

--- src/utils/handle-auto-launch.utils.ts
+++ src/utils/handle-auto-launch.utils.ts
@@ -11,28 +11,31 @@ export const appAutoLauncher = new autoLauncher(autoLauncherOptions)
 /**
  * Disables the auto launch of the app.
  */
-export const disableAutoLaunch = (): void => {
-  appAutoLauncher.isEnabled().then((isEnabled: boolean) => {
-    if (!isEnabled) return;
-    appAutoLauncher.disable().then(() => {
-      console.log("Auto lauch removed successfully!")
-    }).catch((err) => {
-      console.error(err)
+export function disableAutoLaunch(): void {
+    appAutoLauncher.isEnabled().then((isEnabled: boolean) => {
+        if (!isEnabled)
+            return
+        appAutoLauncher.disable().then(() => {
+            console.log("Auto lauch removed successfully!")
+        }).catch((err) => {
+            console.error(err)
+        })
     })
-  })
 }
 
 /**
  * Enables the auto launch of the app.
  */
-export const enableAutoLaunch = (): void => {
-  if (process.env.NODE_ENV === "development") return;
-  appAutoLauncher.isEnabled().then((isEnabled: boolean) => {
-    if (isEnabled) return;
-    appAutoLauncher.enable().then(() => {
-      console.log("Auto lauch created successfully!")
-    }).catch((err) => {
-      console.error(err)
+export function enableAutoLaunch(): void {
+    if (process.env.NODE_ENV === "development")
+        return
+    appAutoLauncher.isEnabled().then((isEnabled: boolean) => {
+        if (isEnabled)
+            return
+        appAutoLauncher.enable().then(() => {
+            console.log("Auto lauch created successfully!")
+        }).catch((err) => {
+            console.error(err)
+        })
     })
-  })
 }
